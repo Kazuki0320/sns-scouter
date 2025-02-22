aa42e9c198685a7299f1b5757dee241873209754
ittyi <vioiv.k4@gmail.com>
2025-02-22 16:27:38 +0900

    ✨ add: 実験用のページを作成

---
+++ src/app/experiment/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <>
+            <h2>Experiment Page</h2>
+            <h3>例えば、ここでコンポーネントのボタンを置いて挙動のテストをする</h3>
+            <h3>例えば、ここでフォロワー数取得のロジックを置いて挙動のテストをする</h3>
+            <p>hello, world</p>
+        </>
+    );
+}
