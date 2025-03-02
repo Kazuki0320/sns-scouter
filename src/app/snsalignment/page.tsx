a7864234cf8f8381cb1d0b652087910cb584ce78
ittyi <vioiv.k4@gmail.com>
2025-03-02 11:27:25 +0900

    ✨ fix: 既存の form フォルダの命名を、実態に合わせて Sns Alignment に変更

--- src/app/snsalignment/page.tsx
+++ src/app/snsalignment/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <>
-            <h2>Form Page</h2>
+            <h2>Sns Alignment Page</h2>
             <h3>ここで診断用に必要なフォーム入力があればこのページを使う</h3>
         </>
     );
