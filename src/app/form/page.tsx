54d8c1d6f4c3d5c4cfeb6926b61f93fe98719e59
myo-i <64891275+myo-i@users.noreply.github.com>
2025-03-06 22:09:38 +0900

    Update src/app/form/page.tsx
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -7,12 +7,8 @@ export default function Page() {
     <>
       <h2>Form Page</h2>
       <h3>ここで診断用に必要なフォーム入力があればこのページを使う</h3>
-      <button
-        onClick={() => {
-          router.push("/result");
-        }}
-      >
-        結果ページへ移動
+      <button>
+          <Link href={"/result"}>結果ページへ移動</Link>
       </button>
     </>
   );
