2f052c5b7fde6632d8abc441d9fdcf6bc6bb2edb
myo-i <Imiyo0521@gmail.com>
2025-03-06 21:44:07 +0900

    feat: formからresultにロード画面を挟んでページ遷移

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,8 +1,20 @@
+"use client"; // useRouterを使うために必要
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-    return (
-        <>
-            <h2>Form Page</h2>
-            <h3>ここで診断用に必要なフォーム入力があればこのページを使う</h3>
-        </>
-    );
+  const router = useRouter();
+
+  return (
+    <>
+      <h2>Form Page</h2>
+      <h3>ここで診断用に必要なフォーム入力があればこのページを使う</h3>
+      <button
+        onClick={() => {
+          router.push("/result");
+        }}
+      >
+        結果ページへ移動
+      </button>
+    </>
+  );
 }
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,8 +1,19 @@
-export default function Page() {
+async function fetchData() {
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // 3秒待つ
+    return {message: "SNSの結果表示"};
+}
+
+export default async function Page() {
+    const data = await fetchData(); // データ取得のシミュレーション
     return (
         <>
             <h2>Result Page</h2>
-            <h3>ここでスカウターの測定結果を表示する。Xへのシェアボタンもここに配置</h3>
+            <h3>
+                ここでスカウターの測定結果を表示する。Xへのシェアボタンもここに配置
+            </h3>
+            <h3>
+                {data.message}
+            </h3>
         </>
     );
 }
