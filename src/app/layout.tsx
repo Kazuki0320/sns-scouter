3a643a10ceb81e793c4f8cceb7c41ac6fe869cb7
Kazuki0320 <107445735+Kazuki0320@users.noreply.github.com>
2025-03-15 14:06:31 +0900

    Update src/app/layout.tsx
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    router.push('/start');
+    router.push('/result');
   };
   return (
     <html lang="ja">
