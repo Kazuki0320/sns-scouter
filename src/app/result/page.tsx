657bee723b817bce31aa27adcb12de3d0d085298
ittyi <vioiv.k4@gmail.com>
2025-03-16 22:02:35 +0900

    🩹 fix: 不要なコードを削除し、コメントを意図通りに修正

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,13 +1,13 @@
 import { ShareButton } from '@/app/button';
 
 async function fetchData() {
-  // NOTE: ローデイング画面を確認するための一時的な処理
-  await new Promise((resolve) => setTimeout(resolve, 4000)); // 3秒待つ
+  // NOTE: ローディング画面を一定時間表示するための一時的な処理
+  await new Promise((resolve) => setTimeout(resolve, 4000));
   return { message: 'SNSの結果表示' };
 }
 
 export default async function Page() {
-  const data = await fetchData(); // データ取得のシミュレーション
+  const data = await fetchData();
   return (
     <>
       <h2>Result Page</h2>
