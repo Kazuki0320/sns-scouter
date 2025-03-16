ec72b0bbaf9fbabfa853d4b209e0d8cbb9c43420
ittyi <vioiv.k4@gmail.com>
2025-03-16 20:41:52 +0900

    🩹 fix: ローディング画面の長さを微調整

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,8 +1,8 @@
-import {ShareButton} from '@/app/button';
+import { ShareButton } from '@/app/button';
 
 async function fetchData() {
   // NOTE: ローデイング画面を確認するための一時的な処理
-  await new Promise((resolve) => setTimeout(resolve, 3000)); // 3秒待つ
+  await new Promise((resolve) => setTimeout(resolve, 4000)); // 3秒待つ
   return { message: 'SNSの結果表示' };
 }
 
@@ -11,10 +11,8 @@ export default async function Page() {
   return (
     <>
       <h2>Result Page</h2>
-      <h3>
-        ここでスカウターの測定結果を表示する。
-      </h3>
-      <ShareButton text="test"/>
+      <h3>ここでスカウターの測定結果を表示する。</h3>
+      <ShareButton text="test" />
       <h3>{data.message}</h3>
     </>
   );
