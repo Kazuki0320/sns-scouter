00ca511c95737468aa30b997a70d174e72082afa
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-17 23:28:52 +0900

    useEffectを利用して、非同期的にローディングを表示

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -2,10 +2,23 @@
 
 import { ShareButton } from '@/components/ui/Button';
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Loading from './loading';
 
 export default function Page() {
+  const [loading, setLoading] = useState(true);
   const battlePowerResult = useSearchParams();
   const followers = Number(battlePowerResult.get('score'));
+
+  useEffect (() => {
+    const timer = setTimeout(() => setLoading(false), 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return Loading()
+  }
+
   return (
     <>
       <h2>Result Page</h2>
