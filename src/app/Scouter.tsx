1b2a4ba8586f17a0141d44b9d6a8e55c8fd588ea
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-25 23:11:31 +0900

    Blenderで作成したスカウターを追加

---
+++ src/app/Scouter.tsx
@@ -0,0 +1,22 @@
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+
+function Model() {
+  const gltf = useGLTF('/scouter.glb');
+  return <primitive object={gltf.scene} scale={1} />;
+}
+
+export function App() {
+  return (
+    <Canvas 
+      camera={{ position: [0, 2, 5], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  );
+}
