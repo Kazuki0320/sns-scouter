aff4efb767b17ab8879d745a1fe9cb7e5bbc8890
k-naoyuki <remington.m1911ma1@gmail.com>
2025-03-26 11:18:38 +0900

    fix:全部コメントにしたらダメなのかな？とりあえずボタン関連の処理だけ削除してみた。これでだめだったらお手上げ

--- src/app/experiment/page.tsx
+++ src/app/experiment/page.tsx
@@ -1,21 +1,18 @@
-// import React from 'react';
-// import { Button, createButtonProps } from '@/components/ui/Button';
+import React from 'react';
 
-// export default function Page() {
-//   const buttonProps = createButtonProps('button', 'Submit', 'lightblue');
+export default function Page() {
 
-//   return (
-//     <>
-//       <h2>Experiment Page</h2>
-//       <h3>例えば、ここでコンポーネントのボタンを置いて挙動のテストをする</h3>
-//       <Button button={buttonProps} />
-//       <h3>
-//         例えば、ここでフォロワー数取得のロジックを置いて挙動のテストをする
-//       </h3>
-//       <p>hello, world</p>
-//       <div className="mx-auto p-4">
-//         <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h2>Experiment Page</h2>
+      <h3>例えば、ここでコンポーネントのボタンを置いて挙動のテストをする</h3>
+      <h3>
+        例えば、ここでフォロワー数取得のロジックを置いて挙動のテストをする
+      </h3>
+      <p>hello, world</p>
+      <div className="mx-auto p-4">
+        <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+      </div>
+    </>
+  );
+}
