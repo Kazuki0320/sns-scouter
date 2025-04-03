bb4b53208647407a588f8851bebfea7d6a1e7c30
myo-i <Imiyo0521@gmail.com>
2025-04-03 22:06:29 +0900

    feat: 吹き出し実装

---
+++ src/components/ui/SpeechBubble.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '@/components/ui/speechBubble.module.css';
+
+
+export function SpeechBubble() {
+  return (
+    <div className={styles.speechBubble}></div>
+  );
+}
+
