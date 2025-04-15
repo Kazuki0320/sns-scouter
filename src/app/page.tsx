8f9e786e5eef0563f834bedabad753d27ce039a2
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-04-16 00:59:39 +0900

    useEffectにコメント追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
   const [subtitleAnimated, setSubtitleAnimated] = useState(false);
   const [showForm, setShowForm] = useState(false);
 
+  // タイトルアニメーション
   useEffect(() => {
     const timer = setTimeout(() =>{
       setTitleAnimated(true);
