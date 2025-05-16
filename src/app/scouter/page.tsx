f501fbd0357826ca9bca82f8e72567fd4c309678
Kazuki0320 <107445735+Kazuki0320@users.noreply.github.com>
2025-05-16 09:26:34 +0900

    Update src/app/scouter/page.tsx
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- src/app/scouter/page.tsx
+++ src/app/scouter/page.tsx
@@ -65,6 +65,7 @@ export default function ScouterPage() {
   useEffect(() => {
     if (isRedirecting) {
       router.push('/');
+      sessionStorage.removeItem('follower');
     }
   }, [isRedirecting, router]);
 
